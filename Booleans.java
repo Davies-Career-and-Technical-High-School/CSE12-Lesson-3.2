f6d1c06b232094541c45f0cb861cb540c92c388b
Joe Mazzone <jmazzone@daviestech.org>
2020-11-12 23:40:46 +0000

    update

--- Booleans.java
+++ Booleans.java
@@ -1,9 +1,16 @@
-
+import java.util.Scanner;
 
 class Booleans {
     public static void main(String[] args) {
         
-        
+        Scanner scan = new Scanner (System.in);
+
+        int grade = scan.nextInt();
+
+        if(grade >= 80 && grade < 90)
+        {
+            System.out.println("B");
+        }
 
     }
 }
--- ShortCircuit.java
+++ ShortCircuit.java
@@ -1,9 +1,16 @@
-
+import java.util.Scanner;
 
 class ShortCircuit {
     public static void main(String[] args) {
         
-        
+        Scanner scan = new Scanner (System.in);
+         
+        int y = scan.nextInt();
+
+        if ( y < 0 || y > 100)
+        {
+            System.out.println( "Not a valid grade.");
+        }
 
     }
 }
